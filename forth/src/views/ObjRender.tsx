0d854e7369c270104c1f312c136cea4aa0a3b0dc
吴聿轩 <929003281@qq.com>
2019-05-16 10:42:31 +0800

    small polish

--- forth/src/views/ObjRender.tsx
+++ forth/src/views/ObjRender.tsx
@@ -5,5 +5,5 @@ interface IObjRender {
 }
 
 export default function ObjRender({ obj }: IObjRender) {
-  return <ul>{Object.keys(obj).map((v, i) => <li key={i}>{v}: {obj[v]}<br /></li>)}</ul>;
+  return <ul>{Object.keys(obj).map((v, i) => <li key={i}>{v}: {obj[v]}</li>)}</ul>;
 }
